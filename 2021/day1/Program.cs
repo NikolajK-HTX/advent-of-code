532bfaafe1eb71294d565c089fc4fa83cc642265
Nikolaj J-K <kagenogmig@gmail.com>
2021-12-11 12:03:22 +0100

    Template

---
+++ 2021/day1/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+namespace Day1
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            WriteLine("Hello, World!");
+
+        }
+
+    }
+}
+
