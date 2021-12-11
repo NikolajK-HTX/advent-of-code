04b5c2269178a575a932ccbb6c40f2f7594e9f51
Nikolaj J-K <kagenogmig@gmail.com>
2021-12-11 14:59:54 +0100

    Day 2. Part 1 works.

---
+++ 2021/day2/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+StreamReader inputFile = File.OpenText("input.txt");
+
+string[] inputText = inputFile.ReadToEnd().Split('\n');
+
+int horizontalMovement = 0;
+int depth = 0;
+
+foreach (string entireCommand in inputText)
+{
+    if(entireCommand == "")
+    {
+        continue;
+    }
+    
+    string[] commandArray = entireCommand.Split(' ');
+
+    string command = commandArray[0];
+    int moves = int.Parse(commandArray[1]);
+
+    if (command.ToLower() == "forward")
+    {
+        horizontalMovement += moves;
+    }
+    if (command.ToLower() == "up")
+    {
+        depth -= moves;
+    }
+    if (command.ToLower() == "down")
+    {
+        depth += moves;
+    }
+}
+
+Console.WriteLine($"Final horizontal movement: {horizontalMovement}");
+Console.WriteLine($"Final depth: {depth}\n");
+
+Console.WriteLine($"horizontalMovement * depth = {horizontalMovement * depth}");
