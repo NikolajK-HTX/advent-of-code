5f97460d38b63c98a1dc499962f43343c9ba32f2
Nikolaj J-K <kagenogmig@gmail.com>
2021-12-14 13:22:46 +0100

    Day 4 part 1 :-)

---
+++ 2021/day4/Program.cs
@@ -0,0 +1,107 @@
+﻿void checkNumbers(int[] chosenNumbers, List<int[][]> fields, List<int[][]> markedFields, int boardSize)
+{
+    for (int i = 0; i < chosenNumbers.Length; i++)
+    {
+        int chosenNumber = chosenNumbers[i];
+        for (int j = 0; j < fields.Count; j++)
+        {
+            int[][] board = fields[j];
+            for (int x = 0; x < boardSize; x++)
+            {
+                for (int y = 0; y < boardSize; y++)
+                {
+                    if (board[x][y] == chosenNumber)
+                    {
+                        markedFields[j][x][y] = 1;
+                    }
+                }
+            }
+            int won = winningBoard(board, markedFields[j], boardSize);
+            if (won > 0)
+            {
+                Console.WriteLine($"Bingo! {won*chosenNumber}");
+                return;
+            }
+        }
+    }
+}
+
+int winningBoard(int[][] board, int[][] markedBoard, int boardSize)
+{
+    for (int i = 0; i < board.Length; i++)
+    {
+        int[] row = board[i];
+
+        int sumRow = 0;
+        for (int j = 0; j < boardSize; j++)
+        {
+            sumRow += markedBoard[j][i];
+        }
+
+        if (markedBoard[i].Sum() == boardSize || sumRow == boardSize)
+        {
+            int score = calculateSum(board, markedBoard, boardSize);
+            return score;
+        }
+    }
+    return -1;
+}
+
+int calculateSum(int[][] board, int[][] markedBoard, int boardSize)
+{
+    int sum = 0;
+    for (int i = 0; i < boardSize; i++)
+    {
+        for (int j = 0; j < boardSize; j++)
+        {
+            if (markedBoard[i][j] == 0)
+            {
+                sum += board[i][j];
+            }
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine("Welcome to day 4!");
+
+string[] inputArray = File.ReadAllLines("input.txt");
+
+int[] chosenNumbers = Array.ConvertAll(inputArray[0].Split(','), i => int.Parse(i));
+
+List<int[][]> fields = new List<int[][]>();
+List<int[][]> markedFields = new List<int[][]>();
+
+int boardSize = inputArray[2].Split().
+        Where(x => x != string.Empty).ToArray().Length;
+int untilNumber = 2 + boardSize;
+int arrayPos = 0;
+
+fields.Add(new int[boardSize][]);
+markedFields.Add(new int[boardSize][]);
+
+for (int i = 2; i < inputArray.Length; i++)
+{
+    if (inputArray[i] == "")
+    {
+        continue;
+    }
+    if (untilNumber < i)
+    {
+        fields.Add(new int[boardSize][]);
+        markedFields.Add(new int[boardSize][]);
+
+        untilNumber += boardSize + 1;
+        arrayPos++;
+
+    }
+    fields[arrayPos][((i - 2) % boardSize)] = Array.ConvertAll(inputArray[i].Split().
+        Where(x => x != string.Empty).ToArray(), s => int.Parse(s));
+    markedFields[arrayPos][((i - 2) % boardSize)] = new int[boardSize];
+    Array.Fill<int>(markedFields[arrayPos][((i - 2) % boardSize)], (int)0);
+}
+
+// play bingo
+checkNumbers(chosenNumbers, fields, markedFields, boardSize);
+
+Console.WriteLine("The end :-)");
