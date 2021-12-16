9a6ac0c24a6ccd4fb4ff888e82c7d6eb3c7c0f24
Nikolaj J-K <kagenogmig@gmail.com>
2021-12-16 12:10:35 +0100

    T'is the solution to part day 7 part 1

---
+++ 2021/day7/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Day 7!");
+
+var inputString = File.ReadAllText("input.txt");
+
+var input = Array.ConvertAll(inputString.Split(','), s => int.Parse(s)).ToList();
+
+input.Sort();
+
+int medianIndex = (int)input.Count/2;
+
+Console.WriteLine($"The median is {input[medianIndex]} with the index of {medianIndex}");
+
+int fuelExpended = 0;
+
+for (int i = 0; i < input.Count; i++)
+{
+    var horizontalPosition = input[i];
+    fuelExpended += Math.Abs(horizontalPosition-input[medianIndex]);
+}
+
+Console.WriteLine($"Total fuel expended is {fuelExpended}");
