0e617058249f41ba7c53c1365ae7e745372d85f1
Nikolaj J-K <kagenogmig@gmail.com>
2021-12-16 13:20:33 +0100

    idk man :-( - it works

--- 2021/day7/Program.cs
+++ 2021/day7/Program.cs
@@ -1,21 +1,51 @@
 ﻿Console.WriteLine("Day 7!");
 
+ulong MyFunction(ulong number)
+{
+    ulong answer = 0;
+    for (ulong i = 1; i <= number; i++)
+    {
+        answer += i;
+    }
+    return answer;
+}
+
 var inputString = File.ReadAllText("input.txt");
 
 var input = Array.ConvertAll(inputString.Split(','), s => int.Parse(s)).ToList();
 
 input.Sort();
 
-int medianIndex = (int)input.Count/2;
-
-Console.WriteLine($"The median is {input[medianIndex]} with the index of {medianIndex}");
-
-int fuelExpended = 0;
+Dictionary<int, ulong> dictInputFuel = new Dictionary<int, ulong>();
 
 for (int i = 0; i < input.Count; i++)
 {
-    var horizontalPosition = input[i];
-    fuelExpended += Math.Abs(horizontalPosition-input[medianIndex]);
+    var inputValue = input[i];
+    if (dictInputFuel.ContainsKey(inputValue))
+    {
+        dictInputFuel[inputValue]++;
+    }
+    else
+    {
+        dictInputFuel[inputValue] = 1;
+    }
 }
 
-Console.WriteLine($"Total fuel expended is {fuelExpended}");
+List<ulong> fuelExpended = new List<ulong>();
+
+for (int j = 0; j < input.Last(); j++)
+{
+    ulong sum = 0;
+    for (int i = 0; i < dictInputFuel.Keys.Count; i++)
+    {
+        int keyValue = dictInputFuel.Keys.ToArray()[i];
+        ulong fuel = MyFunction((ulong)Math.Abs(keyValue - j)) * dictInputFuel[keyValue];
+        sum += fuel;
+    }
+    fuelExpended.Add(sum);
+}
+
+List<ulong> fuelExpendedCopy = new List<ulong>(fuelExpended);
+fuelExpendedCopy.Sort();
+
+Console.WriteLine($"Total fuel expended is {fuelExpendedCopy.First()}");
